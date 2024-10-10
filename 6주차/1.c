fcfb13b90a66dc41b32a1e56278442bbb2f96517
imclaremont <149583958+imclaremont@users.noreply.github.com>
2024-10-10 09:29:08 +0900

    Update 1.c

--- 6주차/1.c
+++ 6주차/1.c
@@ -2,35 +2,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int binarySearch(int* arr, int lowIdx, int highIdx, int key);
+int* arr;
+int binarySearch(int lowIdx, int highIdx, int key);
 
 int main() {
     int n, key;
-    int* arr;
 
     scanf("%d %d", &n, &key);
     arr = (int*)malloc(n * sizeof(int)); 
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
-    printf(" %d", binarySearch(arr, 0, n - 1, key)); 
-    
+    printf(" %d", binarySearch(0, n - 1, key)); 
     free(arr);
+
     return 0;
 }
 
-int binarySearch(int* arr, int lowIdx, int highIdx, int key) {
+int binarySearch(int lowIdx, int highIdx, int key) {
     if (lowIdx > highIdx) {
-        if (highIdx < 0) return -1; // 찾고자 하는 값보다 작은 값이 없는 경우
+        if (highIdx < 0) { // 찾고자 하는 값보다 작은 값이 없는 경우
+            return -1; 
+        }
         return highIdx; // 가장 가까운 작은 값의 인덱스를 반환
     }
 
     int midIdx = lowIdx + (highIdx - lowIdx) / 2;
     if (arr[midIdx] > key) { // 중간값이 키값보다 큰 경우, 왼쪽 절반을 탐색
-        return binarySearch(arr, lowIdx, midIdx - 1, key);
+        return binarySearch(lowIdx, midIdx - 1, key);
     }
     else if (arr[midIdx] < key) { // 중간값이 키값보다 작은 경우, 오른쪽 절반을 탐색
-        return binarySearch(arr, midIdx + 1, highIdx, key);
+        return binarySearch(midIdx + 1, highIdx, key);
     } 
     else if (arr[midIdx] == key) {
         return midIdx;
